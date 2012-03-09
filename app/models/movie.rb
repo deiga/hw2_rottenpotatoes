2de73f027931a10e7e03c6de07fc4167a29455bd
Timo Sand <timo.sand@kapsi.fi>
2012-03-09 12:25:39 +0200

    Added removing of duplicates

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,6 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     ratings = []
     self.select(:rating).uniq.each { |model| ratings << model.rating.to_s }
-    ratings
+    ratings & ratings
   end
 end
